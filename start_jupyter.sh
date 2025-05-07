7bc1f434c28c7d256dc11597ec37e53967d0b738
ShadyEgypt <shadyrafat60@gmail.com>
2025-05-07 16:02:45 +0200

    update assignment

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -4,5 +4,4 @@ export PYTHONPATH=/home/shady/Documents/unige_robotics_msc/0x02_second_year_2nd_
 
 source /opt/ros/noetic/setup.bash
 source /home/shady/Documents/unige_robotics_msc/0x02_second_year_2nd_semster/rt2/rt_ros_ws/devel/setup.bash
-jupyter nbextension enable --py widgetsnbextension --user
-jupyter notebook
+jupyter notebook --allow-root
